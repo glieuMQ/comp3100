c33c59cf14937fc50f8b3771d48d50b3a28065ab
Garry Lieu <garry.lieu@students.mq.edu.au>
2023-03-31 23:48:36 +1100

    LRR implemented and client-server interactions look correct, but tests provided in w6 workshop failing

--- stage1/MyClient.java
+++ stage1/MyClient.java
@@ -26,66 +26,167 @@ class MyClient {
         response = in.readLine(); // receive OK
         System.out.println("Server: " + response);
 
-        while(!response.equals("NONE")){
-            message = "REDY";
-            out.println(message);
-            System.out.println("Client: " + message);
-            response = in.readLine();
-            System.out.println("Server: " + response);
+        //String job = "JOBN";
+        message = "REDY";
+        out.println(message);
+        System.out.println("Client: " + message);
+        response = in.readLine();
+        String job = new String(response);
+        System.out.println("Server: " + response);
 
-            String job = new String(response);
-            System.out.println(job);
 
-            message = "GETS All";
-            out.println(message);
-            System.out.println("Client: " + message);
-            response = in.readLine();
-            System.out.println("Server: " + response);
+        int largestServCores = 0;
+        int largestServNum = 0;
+        String largestServType = "";
+        int servID = 0;
+        String currServType = "";
+        int currServCores = 0;
 
-            String[] servInfo = response.split(" ");
-            int nRecs = Integer.parseInt(servInfo[1]);
+        while(!job.equals("NONE")){
+            if(largestServType.isEmpty()){
+                message = "GETS All";
+                out.println(message);
+                System.out.println("Client: " + message);
+                response = in.readLine();
+                System.out.println("Server: " + response);
 
-            message = "OK";
-            out.println(message);
-            System.out.println("Client: " + message);
-            response = in.readLine();
-            System.out.println("Server: " + response);
+                String[] dataArr = response.split(" ");
+                int nRecs = Integer.parseInt(dataArr[1]);
 
-            int nCores = 0;
-            String sType = "";
-            String[] servSpecs;
-
-            for(int i = 0; i < nRecs; ++i) {
-                servSpecs = response.split(" ");
-                if(Integer.parseInt(servSpecs[4]) >= nCores){
-                    nCores = Integer.parseInt(servSpecs[4]);
-                    sType = servSpecs[0];
-                }
                 message = "OK";
                 out.println(message);
                 System.out.println("Client: " + message);
                 response = in.readLine();
                 System.out.println("Server: " + response);
+
+                String[] servSpecs = response.split(" ");
+                largestServCores = Integer.parseInt(servSpecs[4]);
+                largestServType = servSpecs[0];
+                largestServNum++;
+
+                for(int i = 0; i < nRecs-1; i++){
+                    response = in.readLine();
+                    System.out.println("Server: " + response);
+                    servSpecs = response.split(" ");
+                    
+                    currServCores = Integer.parseInt(servSpecs[4]);
+                    currServType = servSpecs[0];
+
+                    if(currServCores == largestServCores){
+                        largestServNum++;
+                    }
+
+                    if(currServCores > largestServCores){
+                        largestServCores = currServCores;
+                        largestServType = currServType;
+                        largestServNum = 1;
+                    }
+                }
+
+                message = "OK";
+                out.println(message);
+                System.out.println("Client: " + message);
+                response = in.readLine();
+                System.out.println("Server: " + response);
+
+                System.out.println("Largest Server Info: " + largestServCores + " " + largestServNum + " " + largestServType);
             }
 
-            System.out.println("Checkpoint: " + nCores + ", " + sType);
+            String[] jobDetails = job.split(" ");
+            
+            if(servID >= largestServNum){
+                servID = 0;
+            }
 
-            message = "OK";
-            out.println(message);
-            System.out.println("Client: " + message);
-            response = in.readLine();
-            System.out.println("Server: " + response);
-
-            String[] jobArr = job.split(" ");
-            System.out.println(job);
-            if(jobArr[0].equals("JOBN")){
-                message = "SCHD " + jobArr[2] + " " + sType + " 0";
+            if(jobDetails[0].equals("JOBN")){
+                message = "SCHD " + jobDetails[2] + " " + largestServType + " " + servID;
+                servID++;
                 out.println(message);
                 System.out.println("Client: " + message);
                 response = in.readLine();
                 System.out.println("Server: " + response);
             }
+
+            message = "REDY";
+            out.println(message);
+            System.out.println("Client: " + message);
+            response = in.readLine();
+            job = new String(response);
+            System.out.println("Server: " + response + "\n---------------------------------------");
         }
+
+/*
+        int nCores = 0;
+        int nServs = 0;
+        String largestServType = "";
+        String[] servSpecs;
+        int counter = 0; // counter used for round-robin job scheduling
+
+        while(!job.equals("NONE")){
+            // determine largest server type and number available
+            if (largestServType.isEmpty()){
+                message = "GETS All";
+                out.println(message);
+                System.out.println("Client: " + message);
+                response = in.readLine();
+                System.out.println("Server: " + response);
+
+                String[] servInfo = response.split(" ");
+                int nRecs = Integer.parseInt(servInfo[1]);
+
+                message = "OK";
+                out.println(message);
+                System.out.println("Client: " + message);
+                response = in.readLine();
+                System.out.println("Server: " + response);
+
+                for(int i = 0; i < nRecs-1; i++) {
+                    servSpecs = response.split(" ");
+
+                    if(Integer.parseInt(servSpecs[4]) > nCores){
+                        nCores = Integer.parseInt(servSpecs[4]);
+                        largestServType = servSpecs[0];
+                        nServs = 1;
+                    }
+
+                    if(Integer.parseInt(servSpecs[4]) == nCores){
+                        nServs++;
+                    }
+
+                    message = "OK";
+                    out.println(message);
+                    System.out.println("Client: " + message);
+                    response = in.readLine();
+                    System.out.println("Server: " + response);
+                }
+            }
+
+            String[] jobDetails = job.split(" ");
+            
+            if(counter >= nServs){
+                counter = 0;
+            }
+
+            if(jobDetails[0].equals("JOBN")){
+                message = "SCHD " + jobDetails[2] + " " + largestServType + " " + counter;
+                counter++;
+                out.println(message);
+                System.out.println("Client: " + message);
+                response = in.readLine();
+                System.out.println("Server: " + response);
+            }
+
+            message = "REDY";
+            out.println(message);
+            System.out.println("Client: " + message);
+            response = in.readLine();
+            job = new String(response);
+            System.out.println("Server: " + response + "\n---------------------------");
+
+            
+            //System.out.println(job);
+        }
+*/
 
         message = "QUIT";
         out.println(message);

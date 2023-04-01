42c3b3c04556df13ba8f1fceecd53700dd6f3062
Garry Lieu <garry.lieu@students.mq.edu.au>
2023-04-01 14:41:57 +1100

    Comments completed and code block for finding largest server taken out of while loop

--- stage1/MyClient.java
+++ stage1/MyClient.java
@@ -1,3 +1,9 @@
+/*
+ * AUTHOR: Garry Lieu
+ * SID: 46629297
+ * DATE: 1/4/2023
+ */
+
 import java.net.*;
 import java.io.*;
 
@@ -10,190 +16,130 @@ class MyClient {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-        String message, response;
-
-        // send HELO
-        message = "HELO";
-        out.println(message);
-        System.out.println("Client: " + message);
-        response = in.readLine(); // receive OK
-        System.out.println("Server: " + response);
-
-        // send AUTH <user>
-        message = "AUTH garry";
-        out.println(message);
-        System.out.println("Client: " + message);
-        response = in.readLine(); // receive OK
-        System.out.println("Server: " + response);
-
-        //String job = "JOBN";
-        message = "REDY";
-        out.println(message);
-        System.out.println("Client: " + message);
-        response = in.readLine();
-        String job = new String(response);
-        System.out.println("Server: " + response);
-
-
-        int largestServCores = 0;
-        int largestServNum = 0;
-        String largestServType = "";
-        int servID = 0;
-        String currServType = "";
+        // VARIABLES ---------------------------------------------------------------------------------------
+        String send, receive; // stores messages to and responses from the client and server
+        int largestServCores = 0; // number of cores in largest server
+        int largestServNum = 0; // quantity of the largest server type
+        String largestServType = ""; // stores largest server type
+        int servID = 0; // counter variable used to perform the round robin
+        String currServType = ""; // variables used for searching.
         int currServCores = 0;
 
-        while(!job.equals("NONE")){
-            if(largestServType.isEmpty()){
-                message = "GETS All";
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
+        // INITIAL HANDSHAKE --------------------------------------------------------------------------------
+        // send HELO
+        send = "HELO";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine(); // receive OK
+        System.out.println("Server: " + receive);
 
-                String[] dataArr = response.split(" ");
-                int nRecs = Integer.parseInt(dataArr[1]);
+        // send AUTH <user>
+        send = "AUTH garry";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine(); // receive OK
+        System.out.println("Server: " + receive);
 
-                message = "OK";
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
+        // send REDY
+        send = "REDY";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine();
+        String job = new String(receive); // storing response to REDY so that jobs can be scheduled later on
+        System.out.println("Server: " + receive);
 
-                String[] servSpecs = response.split(" ");
-                largestServCores = Integer.parseInt(servSpecs[4]);
-                largestServType = servSpecs[0];
+        // DETERMINING LARGEST SERVER TYPE AND QUANTITY ----------------------------------------------------
+        // send GETS to query server states
+        send = "GETS All";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine(); // receive DATA nRecs recLen
+        System.out.println("Server: " + receive);
+
+        String[] dataArr = receive.split(" ");
+        int nRecs = Integer.parseInt(dataArr[1]);
+
+        // send OK for DATA
+        send = "OK";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine(); // receive record about first server
+        System.out.println("Server: " + receive);
+
+        // extracting number of cores and server type, and starting count
+        String[] servSpecs = receive.split(" ");
+        largestServCores = Integer.parseInt(servSpecs[4]);
+        largestServType = servSpecs[0];
+        largestServNum++;
+
+        // retrieving records for each server
+        for(int i = 0; i < nRecs-1; i++){
+            receive = in.readLine();
+            System.out.println("Server: " + receive);
+            servSpecs = receive.split(" ");
+            
+            currServCores = Integer.parseInt(servSpecs[4]);
+            currServType = servSpecs[0];
+
+            // increasing count if number of cores in current server match largestServCores and it is of the same type
+            if(currServCores == largestServCores && currServType.equals(largestServType)){
                 largestServNum++;
-
-                for(int i = 0; i < nRecs-1; i++){
-                    response = in.readLine();
-                    System.out.println("Server: " + response);
-                    servSpecs = response.split(" ");
-                    
-                    currServCores = Integer.parseInt(servSpecs[4]);
-                    currServType = servSpecs[0];
-
-                    if(currServCores == largestServCores && currServType.equals(largestServType)){
-                        largestServNum++;
-                    }
-
-                    if(currServCores > largestServCores){
-                        largestServCores = currServCores;
-                        largestServType = currServType;
-                        largestServNum = 1;
-                    }
-                }
-
-                message = "OK";
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
-
-                System.out.println("Largest Server Info: " + largestServCores + " " + largestServNum + " " + largestServType);
             }
 
+            // resetting count if a larger server type is encountered
+            if(currServCores > largestServCores){
+                largestServCores = currServCores;
+                largestServType = currServType;
+                largestServNum = 1;
+            }
+        }
+
+        // send OK after receiving last record
+        send = "OK";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine(); // receive .
+        System.out.println("Server: " + receive);
+        //System.out.println("Largest Server Info: " + largestServCores + " " + largestServNum + " " + largestServType);
+
+        // SCHEDULING JOBS USING LRR --------------------------------------------------------------------
+
+        while(!job.equals("NONE")){
             String[] jobDetails = job.split(" ");
             
+            // cycling back to first server when last has been assigned a job
             if(servID >= largestServNum){
                 servID = 0;
             }
 
+            // schedule job if received JOBN after REDY
             if(jobDetails[0].equals("JOBN")){
-                message = "SCHD " + jobDetails[2] + " " + largestServType + " " + servID;
-                servID++;
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
+                send = "SCHD " + jobDetails[2] + " " + largestServType + " " + servID;
+                servID++; // next largest server
+                out.println(send);
+                System.out.println("Client: " + send);
+                receive = in.readLine();
+                System.out.println("Server: " + receive);
             }
 
-            message = "REDY";
-            out.println(message);
-            System.out.println("Client: " + message);
-            response = in.readLine();
-            job = new String(response);
-            System.out.println("Server: " + response + "\n---------------------------------------");
+            // send REDY for next job
+            send = "REDY";
+            out.println(send);
+            System.out.println("Client: " + send);
+            receive = in.readLine();
+            job = new String(receive);
+            System.out.println("Server: " + receive + "\n---------------------------------------");
         }
 
-/*
-        int nCores = 0;
-        int nServs = 0;
-        String largestServType = "";
-        String[] servSpecs;
-        int counter = 0; // counter used for round-robin job scheduling
+        // TERMINATING CLIENT-SERVER INTERACTION ----------------------------------------------------------
+        // send QUIT
+        send = "QUIT";
+        out.println(send);
+        System.out.println("Client: " + send);
+        receive = in.readLine(); // receive QUIT
+        System.out.println("Server: " + receive);
 
-        while(!job.equals("NONE")){
-            // determine largest server type and number available
-            if (largestServType.isEmpty()){
-                message = "GETS All";
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
-
-                String[] servInfo = response.split(" ");
-                int nRecs = Integer.parseInt(servInfo[1]);
-
-                message = "OK";
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
-
-                for(int i = 0; i < nRecs-1; i++) {
-                    servSpecs = response.split(" ");
-
-                    if(Integer.parseInt(servSpecs[4]) > nCores){
-                        nCores = Integer.parseInt(servSpecs[4]);
-                        largestServType = servSpecs[0];
-                        nServs = 1;
-                    }
-
-                    if(Integer.parseInt(servSpecs[4]) == nCores){
-                        nServs++;
-                    }
-
-                    message = "OK";
-                    out.println(message);
-                    System.out.println("Client: " + message);
-                    response = in.readLine();
-                    System.out.println("Server: " + response);
-                }
-            }
-
-            String[] jobDetails = job.split(" ");
-            
-            if(counter >= nServs){
-                counter = 0;
-            }
-
-            if(jobDetails[0].equals("JOBN")){
-                message = "SCHD " + jobDetails[2] + " " + largestServType + " " + counter;
-                counter++;
-                out.println(message);
-                System.out.println("Client: " + message);
-                response = in.readLine();
-                System.out.println("Server: " + response);
-            }
-
-            message = "REDY";
-            out.println(message);
-            System.out.println("Client: " + message);
-            response = in.readLine();
-            job = new String(response);
-            System.out.println("Server: " + response + "\n---------------------------");
-
-            
-            //System.out.println(job);
-        }
-*/
-
-        message = "QUIT";
-        out.println(message);
-        System.out.println("Client: " + message);
-        response = in.readLine();
-        System.out.println("Server: " + response);
-
+        // closing input/output streams and socket
         in.close();
         out.close();
         clientSocket.close();

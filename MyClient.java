4f1bbad0a9b50815396eb6117f98aba811233a0d
Garry Lieu <garry.lieu@students.mq.edu.au>
2023-05-25 15:41:25 +1000

    First implementation of first fit algorithm, and created function to send messages to and receive responses from server.

--- MyClient.java
+++ MyClient.java
@@ -28,105 +28,82 @@ class MyClient {
 
         // INITIAL HANDSHAKE --------------------------------------------------------------------------------
         // send HELO
-        send = "HELO";
-        out.println(send);
-        receive = in.readLine(); // receive OK
+         // receive OK
+        receive = sendMessage("HELO", in , out);
 
         // send AUTH <user>
-        send = "AUTH garry";
-        out.println(send);
-        receive = in.readLine(); // receive OK
+        // receive OK
+        receive = sendMessage("AUTH garry", in , out);
 
         // send REDY
-        send = "REDY";
-        out.println(send);
-        receive = in.readLine();
+        receive = sendMessage("REDY", in , out);
         String job = new String(receive); // storing response to REDY so that jobs can be scheduled later on
-
-        // DETERMINING LARGEST SERVER TYPE AND QUANTITY ----------------------------------------------------
-        // send GETS to query server states
-        send = "GETS All";
-        out.println(send);
-        receive = in.readLine(); // receive DATA nRecs recLen
-
-        String[] dataArr = receive.split(" ");
-        int nRecs = Integer.parseInt(dataArr[1]);
-
-        // send OK for DATA
-        send = "OK";
-        out.println(send);
-        receive = in.readLine(); // receive record about first server
-
-        // extracting number of cores and server type, and starting count
-        String[] servSpecs = receive.split(" ");
-        serverList = new String[nRecs][servSpecs.length];
-        serverList[0] = servSpecs;
-        largestServCores = Integer.parseInt(servSpecs[4]);
-        largestServType = servSpecs[0]; // storing server information -> not used in stage 1
-        largestServNum++;
-
-        // retrieving records for each server
-        // linear search since records are retrieved one-by-one anyway
-        for(int i = 1; i < nRecs; i++){
-            receive = in.readLine();
-            servSpecs = receive.split(" ");
-            serverList[i] = servSpecs; // storing serving information -> not used in stage 1
-
-            currServCores = Integer.parseInt(servSpecs[4]);
-            currServType = servSpecs[0];
-
-            // increasing count if number of cores in current server match largestServCores and it is of the same type
-            if(currServCores == largestServCores && currServType.equals(largestServType)){
-                largestServNum++;
-            }
-
-            // resetting count if a larger server type is encountered
-            if(currServCores > largestServCores){
-                largestServCores = currServCores;
-                largestServType = currServType;
-                largestServNum = 1;
-            }
-        }
-
-        // send OK after receiving last record
-        send = "OK";
-        out.println(send);
-        receive = in.readLine(); // receive .
-
-        // SCHEDULING JOBS USING LRR --------------------------------------------------------------------
 
         while(!job.equals("NONE")){
             String[] jobDetails = job.split(" ");
-            
-            // cycling back to first server when last has been assigned a job
-            if(servID >= largestServNum){
-                servID = 0;
-            }
 
             // schedule job if received JOBN after REDY
             if(jobDetails[0].equals("JOBN")){
-                send = "SCHD " + jobDetails[2] + " " + largestServType + " " + servID;
-                servID++; // next server of the largest type
-                out.println(send); // receive OK
-                receive = in.readLine();
+                // receive DATA nRecs recLen
+                receive = sendMessage("GETS Available " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6], in , out);
+                
+                String[] dataArr = receive.split(" ");
+                int nRecs = Integer.parseInt(dataArr[1]);
+
+                if(nRecs == 0){
+                    receive = sendMessage("OK", in , out);
+                    receive = sendMessage("GETS Capable " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6], in , out);
+                    dataArr = receive.split(" ");
+                    nRecs = Integer.parseInt(dataArr[1]);
+                }
+
+                // send OK for DATA
+                // receive record about first server
+                receive = sendMessage("OK", in , out);
+
+                System.out.println("Server: " + receive);
+                String servCap = new String(receive);
+                String[] servDetails = servCap.split(" ");
+
+                // retrieving records for each server
+                for(int i = 1; i < nRecs; i++){
+                    receive = in.readLine();
+                    //System.out.println("Server: " + receive);
+                }
+
+                //send OK after receiving last record
+                // receive .
+                receive = sendMessage("OK", in , out);
+
+                // scheduling job
+                receive = sendMessage("SCHD " + jobDetails[2] + " " + servDetails[0] + " " + servDetails[1], in , out);
             }
 
             // send REDY for next job
-            send = "REDY";
-            out.println(send);
-            receive = in.readLine(); // receive next event
+            // receive next event
+            receive = sendMessage("REDY", in , out);
             job = new String(receive);
         }
 
         // TERMINATING CLIENT-SERVER INTERACTION ----------------------------------------------------------
         // send QUIT
-        send = "QUIT";
-        out.println(send);
-        receive = in.readLine(); // receive QUIT
+        receive = sendMessage("QUIT", in , out);
 
         // closing input/output streams and socket
         in.close();
         out.close();
         clientSocket.close();
+    }
+
+    public static String sendMessage(String message, BufferedReader inputStream, PrintWriter outputStream){
+        String response = "";
+        outputStream.println(message);
+        try{
+            response = inputStream.readLine();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        return response;
     }
 }

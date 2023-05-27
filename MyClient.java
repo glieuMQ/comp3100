736825181a43b10175bc1cbadb76a502f3e2585c
Garry Lieu <garry.lieu@students.mq.edu.au>
2023-05-27 12:09:45 +1000

    abstracted first fit algorithm into its own function.

--- MyClient.java
+++ MyClient.java
@@ -8,86 +8,45 @@ import java.net.*;
 import java.io.*;
 
 class MyClient {
+    static BufferedReader in;
+    static PrintWriter out;
+    static String receive;
+
     public static void main(String[] args) throws IOException {
         // creating a socket to connect to server on localhost:50000
         Socket clientSocket = new Socket("localhost", 50000);
 
         // initialising input and output streams for socket
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
 
         // VARIABLES ---------------------------------------------------------------------------------------
-        String send, receive; // stores messages to and responses from the client and server
-        int largestServCores = 0; // number of cores in largest server
-        int largestServNum = 0; // quantity of the largest server type
-        String largestServType = ""; // stores largest server type
-        int servID = 0; // counter variable used to perform the round robin
-        String currServType = ""; // variables used for searching
-        int currServCores = 0;
         String[][] serverList; // holds specs of each server -> not used here. More useful for stage 2
 
         // INITIAL HANDSHAKE --------------------------------------------------------------------------------
         // send HELO
          // receive OK
-        receive = sendMessage("HELO", in , out);
+        receive = sendMessage("HELO");
 
         // send AUTH <user>
         // receive OK
-        receive = sendMessage("AUTH garry", in , out);
+        receive = sendMessage("AUTH garry");
 
         // send REDY
-        receive = sendMessage("REDY", in , out);
-        String job = new String(receive); // storing response to REDY so that jobs can be scheduled later on
+        receive = sendMessage("REDY");
+        String event = new String(receive); // storing response to REDY so that jobs can be scheduled later on
 
-        while(!job.equals("NONE")){
-            String[] jobDetails = job.split(" ");
-
-            // schedule job if received JOBN after REDY
-            if(jobDetails[0].equals("JOBN")){
-                // receive DATA nRecs recLen
-                receive = sendMessage("GETS Available " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6], in , out);
-                
-                String[] dataArr = receive.split(" ");
-                int nRecs = Integer.parseInt(dataArr[1]);
-
-                if(nRecs == 0){
-                    receive = sendMessage("OK", in , out);
-                    receive = sendMessage("GETS Capable " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6], in , out);
-                    dataArr = receive.split(" ");
-                    nRecs = Integer.parseInt(dataArr[1]);
-                }
-
-                // send OK for DATA
-                // receive record about first server
-                receive = sendMessage("OK", in , out);
-
-                System.out.println("Server: " + receive);
-                String servCap = new String(receive);
-                String[] servDetails = servCap.split(" ");
-
-                // retrieving records for each server
-                for(int i = 1; i < nRecs; i++){
-                    receive = in.readLine();
-                    //System.out.println("Server: " + receive);
-                }
-
-                //send OK after receiving last record
-                // receive .
-                receive = sendMessage("OK", in , out);
-
-                // scheduling job
-                receive = sendMessage("SCHD " + jobDetails[2] + " " + servDetails[0] + " " + servDetails[1], in , out);
-            }
-
-            // send REDY for next job
-            // receive next event
-            receive = sendMessage("REDY", in , out);
-            job = new String(receive);
+        while(!event.equals("NONE")){
+            // schedule job following first fit algorithm.
+            ffsched(job);
+            // send REDY for next event. receive next event and store it
+            receive = sendMessage("REDY");
+            event = new String(receive);
         }
 
         // TERMINATING CLIENT-SERVER INTERACTION ----------------------------------------------------------
         // send QUIT
-        receive = sendMessage("QUIT", in , out);
+        receive = sendMessage("QUIT");
 
         // closing input/output streams and socket
         in.close();
@@ -95,15 +54,58 @@ class MyClient {
         clientSocket.close();
     }
 
-    public static String sendMessage(String message, BufferedReader inputStream, PrintWriter outputStream){
+    public static String sendMessage(String message){
         String response = "";
-        outputStream.println(message);
+        out.println(message);
         try{
-            response = inputStream.readLine();
+            response = in.readLine();
         }
         catch(IOException e) {
             e.printStackTrace();
         }
         return response;
+    }
+
+    public static void ffsched(String job){
+        String[] jobDetails = job.split(" ");
+        // schedule job if received JOBN after REDY
+        if(jobDetails[0].equals("JOBN")){
+            // receive DATA nRecs recLen
+            receive = sendMessage("GETS Available " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6]);
+                
+            String[] dataArr = receive.split(" ");
+            int nRecs = Integer.parseInt(dataArr[1]);
+            
+            // if no servers are available, schedule job to first capable one instead
+            if(nRecs == 0){
+                receive = sendMessage("OK");
+                receive = sendMessage("GETS Capable " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6]);
+                dataArr = receive.split(" ");
+                nRecs = Integer.parseInt(dataArr[1]);
+            }
+
+            // send OK for DATA. receive record about first server
+            receive = sendMessage("OK");
+
+            System.out.println("Server: " + receive);
+            String servCap = new String(receive);
+            String[] servDetails = servCap.split(" ");
+
+            // retrieving records for each server
+            for(int i = 1; i < nRecs; i++){
+                try{
+                    receive = in.readLine();
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //send OK after receiving last record. receive .
+            receive = sendMessage("OK");
+
+            // scheduling job
+            receive = sendMessage("SCHD " + jobDetails[2] + " " + servDetails[0] + " " + servDetails[1]);
+        }
     }
 }
